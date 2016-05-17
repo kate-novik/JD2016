8f4206480b2d2b9c9f04492b19a662a280d1674d
Spec <specmedia@icloud.com>
2016-05-18 01:56:29 +0300

    09

---
+++ src/by/it/sergeev/jd01_09/IVariable.java
@@ -0,0 +1,16 @@
+package by.it.sergeev.jd01_09;
+
+/**
+ * Created by Spec on 18.05.16.
+ */
+public interface IVariable {
+    Variable add(Variable varAdd);
+    Variable sub(Variable varSub);
+    Variable mul(Variable varMul);
+    Variable div(Variable varDiv);
+}
+
+
+
+
+
