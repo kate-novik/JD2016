9a186d4039e7853bd751af90fffb92fc9ba0071d
chatovich <chatovich@gmail.com>
2016-05-18 12:25:09 +0300

    jd01_09 MathLab

---
+++ src/by/it/vasilevich/jd01_09/Error.java
@@ -0,0 +1,10 @@
+package by.it.vasilevich.jd01_09;
+
+/**
+ * Created by user_2 on 18.05.2016.
+ */
+public class Error {
+    public Error(String hint){
+        System.out.println("ERR:"+hint);
+    }
+}
---
+++ src/by/it/vasilevich/jd01_09/interfaces/IMul.java
@@ -0,0 +1,9 @@
+package by.it.vasilevich.jd01_09.interfaces;
+
+import by.it.vasilevich.jd01_09.vars.Var;
+/**
+ * Created by user_2 on 18.05.2016.
+ */
+public interface IMul {
+    Var mul (Var var); //умножение
+}
