452b585ff2fef1eb41a8ef1e22e61bd2548da3b0
FunnyGardener <SamyilovD@mail.ru>
2016-05-18 12:14:48 +0300

    17/05/2016

---
+++ src/by/it/kust/jd01_09/interfaces/IDiv.java
@@ -0,0 +1,8 @@
+package by.it.kust.jd01_09.interfaces;
+
+/**
+ * Created by user_2 on 18.05.2016.
+ */
+public interface IDiv {
+    Var div(Var var);
+}
---
+++ src/by/it/kust/jd01_09/interfaces/IVar.java
@@ -0,0 +1,9 @@
+package by.it.kust.jd01_09.interfaces;
+
+/**
+ * Created by user_2 on 18.05.2016.
+ */
+public interface IVar {
+    String toString();
+    void setFrom(String str);
+}
