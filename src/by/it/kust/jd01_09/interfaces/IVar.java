e692e87ef365032ef1120a54f5eccc42c5c9a2ec
tanyakust@gmail.com <sunrise2016>
2016-05-18 21:45:22 +0300

    JD01_06: TaskB(3)

--- src/by/it/kust/jd01_09/interfaces/IVar.java
+++
@@ -1,9 +0,0 @@
-package by.it.kust.jd01_09.interfaces;
-
-/**
- * Created by user_2 on 18.05.2016.
- */
-public interface IVar {
-    String toString();
-    void setFrom(String str);
-}
--- src/by/it/kust/jd01_09/vars/Var.java
+++
@@ -1,7 +0,0 @@
-package by.it.kust.jd01_09.vars;
-
-/**
- * Created by user_2 on 18.05.2016.
- */
-public class Var {
-}
