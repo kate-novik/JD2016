c0ee495350fc10ae822a42e203dbdbb4d65bda4b
Sinkevich <SleePokeR@gmail.com>
2016-05-20 11:16:16 +0300

    Вся папка полностью с заданием jd01_08 для проверки. Правка после чьего-то разрушения классов!!!

--- src/by/it/sinkevich/jd01_05/Util.java
+++ src/by/it/sinkevich/jd01_05/Util.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package by.it.sinkevich.jd01_05;
 
 import java.io.BufferedReader;
@@ -40,43 +39,3 @@ class Util {
         }
     }
 }
-=======
-package by.it.sinkevich.jd01_05;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-/**
- * Набор утилит для задания jd01_05.
- * @author Sinkevich Denis
- */
-class Util {
-
-    static double getDoubleFromConsole() throws IOException {
-        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        String line = consoleReader.readLine();
-        return Double.parseDouble(line);
-    }
-    //Вывод результатов табулирования функции на консоль: double[][] matrix -- матрица состоящая из 2-х строк!!!
-    //Первая строка -- аргументы, вторая строка результат функции; String functionName -- название функции, int cols --
-    //количество колонок для вывода
-    static void printFunctionTable(double[][] matrix, String functionName, int cols) {
-        for (int i = 0; i < matrix[0].length; i++) {
-            System.out.printf("| %S[%-4.1f]=%-7.2f |", functionName, matrix[0][i], matrix[1][i]);
-            if ((i + 1) % cols == 0 || i == matrix[0].length - 1) {
-                System.out.println();
-            }
-        }
-    }
-    //Вывод одномерного массива для задание номер 6
-    static void printFunctionForTask6(double[] array, String arrayName) {
-        System.out.println("━━━━━━━━━━━━━━━━━━━━");
-        System.out.println("Array : " + arrayName + ".");
-        System.out.println("━━━━━━━━━━━━━━━━━━━━");
-        for (int index = 0; index < array.length; index++){
-            System.out.println(String.format("%-1s%-1s%-1s%3s%-20s", "[", index, "]", "= ", array[index]));
-        }
-    }
-}
->>>>>>> Stashed changes
--- src/by/it/sinkevich/jd01_06/Poem.java
+++ src/by/it/sinkevich/jd01_06/Poem.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package by.it.sinkevich.jd01_06;
 
 /**
@@ -54,39 +53,3 @@ class Poem {
         taskC.task3();
     }
 }
-=======
-package by.it.sinkevich.jd01_06;
-
-/**
- * Runner для задания jd01_06
- *
- * @author Sinkevich Denis
- */
-public class Poem {
-
-    public static void main(String[] args) {
-        TaskA taskA = new TaskA();
-
-        System.out.println("Задание №1: ");
-        System.out.println(taskA.task1());
-        System.out.println();
-
-        System.out.println("Задание №2: ");
-        taskA.task2();
-        System.out.println();
-
-        System.out.println("Задание №3: ");
-        System.out.println(taskA.task3());
-        System.out.println();
-
-        TaskB taskB = new TaskB();
-        System.out.println("Задание №4: ");
-        System.out.println(taskB.task1());
-        System.out.println();
-
-        System.out.println("Задание №5: ");
-        taskB.task2();
-        System.out.println();
-    }
-}
->>>>>>> Stashed changes
