1c2408e58ad379a5e27c2deafa56c9e405d35ed5
Sinkevich <SleePokeR@gmail.com>
2016-05-22 11:34:31 +0300

    MathLab. Добавлен класс Parser, который обрабатывает строку чисел с арифметическими операциями. В мейне -- обработка строки после парсера и расчёт выражения с учётом приоритетов.

---
+++ src/by/it/sinkevich/jd01_09/parser/Patterns.java
@@ -0,0 +1,10 @@
+package by.it.sinkevich.jd01_09.parser;
+
+/**
+ * Created by Computer on 21.05.2016.
+ * @author Sinkevich Denis
+ */
+public class Patterns {
+
+    public static String newRegexFloat = "[+-]?(((([1-9][0-9]*)|0)\\.[0-9]+)|(([1-9][0-9]*)|0)|((([1-9][0-9]*)|0)\\.)|(\\.[0-9]+))";
+}
