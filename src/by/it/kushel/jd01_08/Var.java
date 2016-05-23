0a1454ddf26484ae829a2e77f7cbf0d5555f85b7
annakushal <annakirpluk@gmail.com>
2016-05-23 07:41:47 +0300

    add jd01_08 черновик

---
+++ src/by/it/kushel/jd01_08/Var.java
@@ -0,0 +1,32 @@
+package by.it.kushel.jd01_08;
+
+/**
+ * Created by Diomn on 21.05.2016.
+ */
+public abstract class Var implements IOperations {
+
+
+
+    public Var add(Var varAdd) {
+        System.out.println("Error");
+        return null;
+    }
+/*
+    @Override
+    public Var sub(Var varSub) {
+        System.out.println("Error");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var varMul) {
+        System.out.println("Error");
+        return null;
+    }
+
+    @Override
+    public Var div(Var varDiv) {
+        System.out.println("Error");
+        return null;
+    }*/
+}
