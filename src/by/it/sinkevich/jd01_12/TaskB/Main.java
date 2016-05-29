19946aa95c25498ab96b53ff1105bcad47f8494a
Sinkevich <SleePokeR@gmail.com>
2016-05-29 16:23:39 +0300

    jd01_12 TaskB1 completed!

---
+++ src/by/it/sinkevich/jd01_12/TaskB/Main.java
@@ -0,0 +1,17 @@
+package by.it.sinkevich.jd01_12.TaskB;
+
+/**
+ * Main class for testing tasks N1, N2.
+ *
+ * @author Sinkevich Denis
+ */
+class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Задание №1: ");
+        TaskB1 taskB1 = new TaskB1();
+        taskB1.fillMapWithWords();
+        taskB1.printMap();
+    }
+}
