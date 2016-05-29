a70af9c43e959c51457e3223ae219ccacda3b705
Sinkevich <SleePokeR@gmail.com>
2016-05-29 18:55:49 +0300

    jd01_12 TaskB2 completed!

--- src/by/it/sinkevich/jd01_12/TaskB/Main.java
+++ src/by/it/sinkevich/jd01_12/TaskB/Main.java
@@ -13,5 +13,15 @@ class Main {
         TaskB1 taskB1 = new TaskB1();
         taskB1.fillMapWithWords();
         taskB1.printMap();
+
+        System.out.println("\nЗадание №2: ");
+        TaskB2 taskB2 = new TaskB2(100000);
+        System.out.print("Время выполнения задачи при помощи ArrayList в наносекундах: ");
+        long arrayTime = taskB2.processArray();
+        System.out.println(arrayTime);
+        System.out.print("Время выполнения задачи при помощи LinkedList в наносекундах: ");
+        long linkedTime = taskB2.processLinked();
+        System.out.println(linkedTime);
+        System.out.println("ArrayList медленнее, чем LinkedList в " + (double) arrayTime / linkedTime + " раз(а)!");
     }
 }
---
+++ src/by/it/sinkevich/jd01_12/TaskB/TaskB2.java
@@ -0,0 +1,69 @@
+package by.it.sinkevich.jd01_12.TaskB;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * TaskB2 class
+ *
+ * @author Sinkevich Denis
+ */
+class TaskB2 {
+
+    private int number;
+
+    TaskB2(int number) {
+        this.number = number;
+    }
+
+    TaskB2() {
+        this(10000);
+    }
+
+    long processArray() {
+        List<Integer> list = new ArrayList<>(number);
+        for (int i = 1; i <= number; i++) {
+            list.add(i);
+        }
+        return removeElements(list);
+    }
+
+    long processLinked() {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= number; i++) {
+            list.add(i);
+        }
+        return removeElements(list);
+    }
+
+    private long removeElements(List<Integer> list) {
+        //Засекаем время начала метода
+        long start = System.nanoTime();
+        //position используется для правильного перехода итератора после прохода цикла
+        int position = 1;
+        //Цикл выполняем до тех пор, пока не останется один элемент в списке
+        while (list.size() != 1) {
+            ListIterator<Integer> iterator = list.listIterator(position);
+            iterator.next();
+            iterator.remove();
+            while (iterator.hasNext()) {
+                //пропускаем один элемент
+                iterator.next();
+                //Проверяем есть ли следующий элемент, если он есть, то переходим, удаляем и ставим position для следующего
+                //прохода цикла на второй элемент списка, если нету, то на первый
+                if (iterator.hasNext()) {
+                    iterator.next();
+                    iterator.remove();
+                    position = 1;
+                } else {
+                    position = 0;
+                }
+            }
+        }
+        //Засекаем время кокончания метода
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
