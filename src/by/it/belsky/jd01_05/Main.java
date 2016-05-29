a2c5bb6930525e7abf4012468fe03bbdbc2df472
TANKVENERA <Mina_2@mail.ru>
2016-05-29 18:54:43 +0300

    jd01_08

--- src/by/it/belsky/jd01_05/Main.java
+++ src/by/it/belsky/jd01_05/Main.java
@@ -1,3 +1,4 @@
+
 package by.it.belsky.jd01_05;
 
 import javafx.concurrent.Task;
@@ -9,22 +10,23 @@ import static java.lang.StrictMath.atan;
  * Created by misha on 12.05.2016.
  */
 public class Main {
-        public static void main(String args[]) {
+    public static void main(String args[]) {
 //Задание 1
 
-                double z = 0;//необходимо задать любое значение для z
-                double x = 3.24;
-                double m = atan(pow(x, 2));
-                System.out.println("m= " + m);
-                System.out.print("Число, вычисленное первым способом: ");
-                System.out.println(Task1.variantA(z));
-                System.out.print("Число, вычисленное вторым способом: ");
-                System.out.println(Task1.variantB(z));
+        double mainz = 1.8;//необходимо задать любое значение для z
+        double x = 3.24;
+        double m = atan(pow(x, 2));
+        System.out.println("m= " + m);
+        System.out.print("Число, вычисленное первым способом: ");
+        System.out.println(Task1.variantA(mainz));
+        System.out.print("Число, вычисленное вторым способом: ");
+        System.out.println(Task1.variantB(mainz));
 // задание 2
-                double y = 0;
-                System.out.println("Первый способ ввода данных: " + Task2.calculateA(y));
-                System.out.println("Второй способ ввода данных: " + Task2.calculateB(0.21E2));
+        double y = 0;
+        System.out.println("Первый способ ввода данных: " + Task2.calculateA(y));
+        System.out.println("Второй способ ввода данных: " + Task2.calculateB(0.21E2));
 //задание 3
-                Task3.printTable();
-        }
+        Task3.printTable();
+    }
+
 }
