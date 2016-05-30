ebd117f36c27eddb30565e76bc787529b1ab39b4
Spec <specmedia@icloud.com>
2016-05-30 09:38:47 +0300

    jd01_12 TaskB1 испр

--- src/by/it/sergeev/jd01_12/TaskB.java
+++ src/by/it/sergeev/jd01_12/TaskB.java
@@ -9,21 +9,19 @@ import java.util.regex.*;
 public class TaskB {
     public static void createHashMap() {
         StringBuilder text = new StringBuilder(Text.getString());
-        HashMap<String,Integer> hm = new HashMap<String, Integer>();
-        Pattern word= Pattern.compile("[a-zA-Z]+");
-        Matcher m =word.matcher(text);
-        while (m.find()){
-            String key = text.substring(m.start(),m.end());
-            if(hm.containsKey(key)){
-                int value=hm.get(key);
-                value++;
-                hm.put(key,value);
-            }
-            else hm.put(key,1);
-        }
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        Pattern word = Pattern.compile("[a-zA-Z]+");
+        Matcher m = word.matcher(text);
+        while (m.find()) {
+            String key = text.substring(m.start(), m.end());
+            if (hm.containsKey(key)) {
+                hm.put(key, hm.get(key) + 1);
+            } else hm.put(key, 1);
 
-        for (Map.Entry entry : hm.entrySet()) {
-            System.out.println("\"" + entry.getKey() +"\" повторяется "+entry.getValue()+" раз(а)" );
+
+            for (Map.Entry entry : hm.entrySet()) {
+                System.out.println("\"" + entry.getKey() + "\" повторяется " + entry.getValue() + " раз(а)");
             }
         }
     }
+}
