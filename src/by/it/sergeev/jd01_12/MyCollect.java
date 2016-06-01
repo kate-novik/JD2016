66146cd8dd5361b374e5b79488c0734d12e8f587
Spec <specmedia@icloud.com>
2016-06-02 00:05:23 +0300

    jd01_12 TaskС1 + TaskС2

--- src/by/it/sergeev/jd01_12/MyCollect.java
+++ src/by/it/sergeev/jd01_12/MyCollect.java
@@ -28,4 +28,30 @@ public class MyCollect {
         }
         return union;
     }
+
+    public static ArrayList<Object> getCross2(ArrayList<Object> A, ArrayList<Object> B) {
+        ArrayList<Object> cross = new ArrayList<Object>();
+        int c = 0;
+        for ( int i = 0; i < A.size(); i++)
+            for (int j = 0; j < B.size(); j++)
+                if (A.get(i)==B.get(j)) {
+                    cross.add(A.get(i));
+                    c++;
+                    break;
+                }
+        System.out.println("Количество повторов :");
+        System.out.println(c);
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        return cross;
+    }
+    public static ArrayList<Object> getUnion2 (ArrayList<Object> A,ArrayList<Object> B){
+        ArrayList<Object> union =new ArrayList<Object>();
+        union.addAll(0,A);
+        union.addAll(A.size(),B);
+        System.out.println();
+        for (Object a : union) {
+            System.out.print(a + " ");
+        }
+        return union;
+    }
 }
---
+++ src/by/it/sergeev/jd01_12/TaskC.java
@@ -0,0 +1,70 @@
+package by.it.sergeev.jd01_12;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskC {
+    public static TreeMap<Integer, String> createTreeMap(String text) {
+        TreeMap<Integer, String> tm = new TreeMap<>();
+        Pattern word = Pattern.compile("[a-zA-Z]+");
+        Matcher m = word.matcher(text);
+
+        int n = 0;
+        while (m.find()) {
+            n++;
+            String st = m.group();
+            tm.put(n, st);
+        }
+        return tm;
+    }
+
+    public static TreeMap<Integer, String> compressedEncryption(TreeMap<Integer, String> tm) {
+        TreeMap<Integer, String> cryption = new TreeMap<>();
+        for (Map.Entry pair : tm.entrySet())
+            if (!cryption.containsValue(pair.getValue()))
+                cryption.put((Integer) pair.getKey(), (String) pair.getValue());
+        return cryption;
+    }
+
+    public static void printEncryption(TreeMap<Integer, String> tm) {
+        for (Map.Entry pair : tm.entrySet())
+            System.out.print(pair.getValue() + "-" + pair.getKey() + "\t");
+    }
+
+    //C2.
+    public static ArrayList<Object> createArrayListC2() {
+        ArrayList<Object> A = new ArrayList<>();
+        ArrayList<Object> B = new ArrayList<>();
+        System.out.println("A" + " | " + "B");
+        for (int i = 0; i < 11; i++) {
+            int randomA = Util.generateRandom(10);
+            A.add(randomA);
+            int randomB = Util.generateRandom(10);
+            B.add(randomB);
+            System.out.println(A.get(i) + " | " + B.get(i));
+        }
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        MyCollect.getCross2(A, B);
+        MyCollect.getUnion2(A, B);
+        return A;
+    }
+
+    //C3. (в процессе)
+    public static ArrayDeque<Character> createDeque() {
+        ArrayDeque<Character> adq = new ArrayDeque<>();
+        String text = "(dfdfdfd[fdf{sds22}df]f)";
+        for (char a : text.toCharArray()) {
+            if ('(' == a || '[' == a || '{' == a) {
+                adq.push(a);
+                continue;
+            }
+            if (')' == a || ']' == a || '}' == a) {
+                adq.push(a);
+                continue;
+            }
+        }
+        return adq;
+    }
+}
+
