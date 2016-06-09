aadc682ebd4a10ab887b22a437538f301f00c354
VasilevichSergey <03022007sv>
2016-06-09 17:43:28 +0300

    Дом 09.06.2016

---
+++ src/by/it/vasilevich/TEST/Main.java
@@ -0,0 +1,21 @@
+package by.it.vasilevich.TEST;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("\n\tЗАДАНИЕ 2");
+        int s = 3700;
+        Metod.Time(s);
+        System.out.println("\n\tЗАДАНИЕ 3");
+        int a = 27;
+        Metod.EvenOdd(a);
+        System.out.println("\n\tЗАДАНИЕ 4");
+        double r = 1;
+        Metod.Radius(r);
+        System.out.println("\n\tЗАДАНИЕ 4а");
+        a = 2017;
+        Metod.Year(a);
+        System.out.println("\n\tЗАДАНИЕ 5");
+        int x = 102348652;
+        Metod.Rubl(x);
+    }
+}
