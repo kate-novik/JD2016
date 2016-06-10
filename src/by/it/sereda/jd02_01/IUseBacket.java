9672d2066130299a6b226bb6ac8cc98f0f0a40fb
Noviko <helloiamkseniya.novik@gmail.com>
2016-06-10 12:07:35 +0300

    jd02_01: Task A

---
+++ src/by/it/sereda/jd02_01/IUseBacket.java
@@ -0,0 +1,7 @@
+package by.it.sereda.jd02_01;
+
+public interface IUseBacket {
+    void takeBacket(); //взял корзину
+
+    void putGoodsToBacket(); //положил выбранный товар в корзину
+}
---
+++ src/by/it/sereda/jd02_01/Utils.java
@@ -0,0 +1,7 @@
+package by.it.sereda.jd02_01;
+
+public class Utils {
+    static int getTimeOut(int from, int to) {
+        return Math.max(from, 0) + (int) (Math.random() * to + 1);
+    }
+}
