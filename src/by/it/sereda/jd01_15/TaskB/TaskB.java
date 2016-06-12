a7d90b4d96496cfc1c3dcd581e5e192df1799d06
Igor Sereda <sis.public@gmail.com>
2016-06-12 13:10:32 +0300

    Taskk jd01_15: Task A, Task B

--- src/by/it/sereda/jd01_15/TaskB/TaskB.java
+++ src/by/it/sereda/jd01_15/TaskB/TaskB.java
@@ -1,17 +1,19 @@
 package by.it.sereda.jd01_15.TaskB;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 public class TaskB {
+
     public static final String OUT_CR = "OUT" + "CR";
 
     public static void main(String[] arg) {
         String src = System.getProperty("user.dir") + "/src/";
         String cl = TaskB.class.getName();
         System.out.println(cl);
-        String path = src.concat(cl.replaceAll("[.]", "/")).concat(".java");
-        String filename = src + "jd01_15/matrix.txt";
+        String path = src.concat(cl.replaceAll("\\.", "/").concat(".java"));
+        System.out.println(path);
+        String file_out = src + "/by/it/sereda/jd01_15/TaskB/matrix_file.txt";
+
         try {
             FileReader fileReader = new FileReader((path));
             BufferedReader in = new BufferedReader(fileReader);
@@ -20,9 +22,16 @@ public class TaskB {
             while ((line = in.readLine()) != null) {
                 //комментарий 1
                 /* комментарий */
-                code.append(line.replaceAll("(//.*)|(//*.+)", "").concat(OUT_CR));
+                line = line.replaceAll("(/\\*{1,2}.*)|//.*", "").concat(OUT_CR);
+                code.append(line);
             }
-            System.out.println(code.toString().replaceAll(OUT_CR, "\n"));
+            String line_for_print = code.toString().replaceAll(OUT_CR, "\n");
+            try (PrintWriter printer_for_file =
+                         new PrintWriter(
+                                 new FileWriter(file_out, false))) {
+                printer_for_file.println(line_for_print);
+                System.out.print(line_for_print);
+            }
         } catch (java.io.IOException e) {
             System.out.println("Ошибка работы с " + path);
         }
