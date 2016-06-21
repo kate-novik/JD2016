861e48f3be80424b8c83224cd6ebf75ae984fa38
landrin <miss-elena86@mail.ru>
2016-06-21 22:48:10 +0300

    jd02_05 A, B, C

---
+++ src/by/it/drachyova/jd02_05/translator.bat
@@ -0,0 +1,5 @@
+d:
+cd D:\by\JD2016\src\by\it\drachyova\jd02_05
+"C:\Program Files\Java\jdk1.8.0_92\bin\native2ascii.exe" -encoding utf-8 greetings_be_BY.txt greetings_be_BY.properties
+"C:\Program Files\Java\jdk1.8.0_92\bin\native2ascii.exe" -encoding utf-8 greetings_ru_RU.txt greetings_ru_RU.properties
+"C:\Program Files\Java\jdk1.8.0_92\bin\native2ascii.exe" -encoding utf-8 greetings_en_EN.txt greetings_en_EN.properties
