0fa870e8ed954171c6f687faac8fd3fa9facbc1d
chatovich <chatovich@gmail.com>
2016-07-01 11:02:37 +0300

    jd02_10

---
+++ src/by/it/sergeev/jd02_10/Converter/ConverterJsonToXML.java
@@ -0,0 +1,13 @@
+package by.it.sergeev.jd02_10.Converter;
+
+public class ConverterJsonToXML extends AbstractConverter{
+    @Override
+    public void buildConverter(String filename) {
+
+    }
+
+    @Override
+    public String getConverterResult() {
+        return null;
+    }
+}
