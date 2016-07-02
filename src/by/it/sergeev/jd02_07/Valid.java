e0bb2e9e29d5e97322c39ec403837a8959b8c83b
Spec <specmedia@icloud.com>
2016-07-02 18:54:09 +0300

    jd02_06 jd02_07

---
+++ src/by/it/sergeev/jd02_07/Valid.java
@@ -0,0 +1,41 @@
+package by.it.sergeev.jd02_07;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class Valid {
+
+    public static void main(String[] args) {
+
+        String catalogSchema = "src/by/it/seregeev/jd02_07/catalog.xsd";
+        String catalogXML = "src/by/it/seregeev/jd02_07/catalog.xml";
+
+        validate(catalogXML, catalogSchema);
+    }
+
+    private static void validate(String xmlFilePath, String schemaFilePath){
+        String xmlLang = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(xmlLang);
+        File schemaFile = new File(schemaFilePath);
+
+        try {
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            Source source = new StreamSource(xmlFilePath);
+            validator.validate(source);
+            System.out.println("XML " + xmlFilePath + " is valid");
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
