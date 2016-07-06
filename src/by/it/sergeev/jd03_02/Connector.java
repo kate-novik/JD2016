42db990533baa2a3255f861697e1c777ce1c5e99
chatovich <chatovich@gmail.com>
2016-07-06 12:18:22 +0300

    jd03_02

---
+++ src/by/it/sergeev/jd03_02/Connector.java
@@ -0,0 +1,38 @@
+package by.it.sergeev.jd03_02;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+    //Памятка. Корректно держать настройки соединения вне кода (!)
+    private static final String URL_DB =
+            "jdbc:mysql://127.0.0.1:2016/it_academy"
+                    +"?useUnicode=true&characterEncoding=UTF-8";
+    private static final String USER_DB = "root";
+    private static final String PASSWORD_DB = "";
+    static {
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        }
+    private static volatile Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            synchronized (URL_DB) {
+                if (connection == null || connection.isClosed())
+                    connection = DriverManager.getConnection(URL_DB, USER_DB, PASSWORD_DB);
+            }
+        }
+        return connection;
+    }
+}
+
---
+++ src/by/it/sergeev/jd03_02/Runner.java
@@ -0,0 +1,17 @@
+package by.it.sergeev.jd03_02;
+
+import java.sql.SQLException;
+
+public class Runner {
+    public static void main(String[] args){
+        User user = new User(0,"lognames","passa","adwa@er.ry",1);
+        UserCRUD userCRUD = new UserCRUD();
+        try {
+            userCRUD.create(user);
+            System.out.println(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
