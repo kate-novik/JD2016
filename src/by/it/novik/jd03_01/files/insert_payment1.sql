4956c6631c5a1ad600e660e1cd500b588a1a0219
kate-novik <ekat.novik@gmail.com>
2016-07-06 15:46:34 +0300

    Update jd03_01 (edit ConnectorDB)

--- src/by/it/novik/jd03_01/files/insert_payment1.sql
+++ src/by/it/novik/jd03_01/files/insert_payment1.sql
@@ -1,1 +1,1 @@
-INSERT INTO payment (ID, FK_Account_Source, FK_Account_Destination, Description, Amount, Date_P) VALUES ('1', '1', '2', 'Перевод средств', '50', '2016-07-13');
+INSERT INTO payment (ID, FK_Account_Source, FK_Account_Destination, Description, Amount, Paydate) VALUES ('1', '1', '2', 'Перевод средств', '50', '2016-07-13');
