49b50a1b83b7fe303518c52bb55dc8f4d2e9f27d
kate-novik <ekat.novik@gmail.com>
2016-07-06 15:46:34 +0300

    Update jd03_01 (edit ConnectorDB)

--- src/by/it/novik/jd03_01/ConnectorDB.java
+++ src/by/it/novik/jd03_01/ConnectorDB.java
@@ -18,7 +18,8 @@ public class ConnectorDB {
     public static Connection getConnection () throws SQLException {
         //Используем файл ресурсов для создания подключения
         ResourceBundle resourceBundle = ResourceBundle.getBundle("by.it.novik.jd03_01.resources.connection");
-        String url = resourceBundle.getString("db.url");
+        String coding = "?useUnicode=".concat(resourceBundle.getString("db.useUnicode")).concat("&characterEncoding=").concat(resourceBundle.getString("db.encoding"));
+        String url = resourceBundle.getString("db.url").concat(coding);
         String user = resourceBundle.getString("db.user");
         String pass = resourceBundle.getString("db.password");
         return DriverManager.getConnection(url,user,pass);
