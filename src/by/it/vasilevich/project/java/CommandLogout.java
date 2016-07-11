ac6ea04b7391bfde2c60d37187e2c4366ac16e6e
vasilevich <dok_2004@tut.by>
2016-07-11 12:30:32 +0300

    project

---
+++ src/by/it/vasilevich/project/java/CommandLogout.java
@@ -0,0 +1,10 @@
+package by.it.vasilevich.project.java;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CommandLogout implements ActionCommand {
+    @Override
+    public String execute(HttpServletRequest request) {
+        return Action.LOGOUT.inPage;
+    }
+}
