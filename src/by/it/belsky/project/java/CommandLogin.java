4db1c95b695355fba282113d1a73126257a09fa5
TANKVENERA <Mina_2@mail.ru>
2016-07-14 23:49:44 +0300

    Jd01_12 (TaskB1)

--- src/by/it/belsky/project/java/CommandLogin.java
+++ src/by/it/belsky/project/java/CommandLogin.java
@@ -1,13 +1,43 @@
 package by.it.belsky.project.java;
 
+import by.it.belsky.project.java.Beans.User;
+import by.it.belsky.project.java.custom_dao.DAO;
+
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public class CommandLogin implements ActionCommand {
     @Override
     public String execute(HttpServletRequest request) {
-        //DO SOMETHING;
-        //USE DAO
-        //&ETC
-        return Action.LOGIN.inPage;
+        String page = Action.LOGIN.inPage;
+
+        //проверим отправлены ли данные из формы, если нет, то покажем форму
+        if (request.getParameter("Password") == null) {
+            return page;
+        }
+        String password=request.getParameter("Password");   //пароль
+        String login=request.getParameter("Login");   //логин
+
+        if (FormHelper.valid(password) && FormHelper.valid(login)) {
+            DAO dao = DAO.getDAO();
+            List<User> users=dao.user.getAll(
+                    String.format("where Login='%s' and Password='%s'",login,password));
+            User user=null;
+            if (users.size()>0) {
+                user=users.get(0);
+            }
+            if (user==null) {
+                request.setAttribute(
+                        Action.msgMessage,
+                        "Неверные данные повторите ввод.");
+                page = Action.LOGIN.inPage;
+            } else {
+                request.setAttribute(
+                        Action.msgMessage,
+                        "Добро пожаловать, "+user.getLogin());
+                page = Action.LOGIN.okPage;
+            }
+        }
+        return page;
     }
 }
