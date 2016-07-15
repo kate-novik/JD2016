2c901e15e7ef45ec25336a1894762e68c9ea47da
VasilevichSergey <03022007sv>
2016-07-15 08:50:36 +0300

    jd03_03

---
+++ src/by/it/vasilevich/jd03_03/beans/Ad.java
@@ -0,0 +1,17 @@
+package by.it.vasilevich.jd03_03.beans;
+
+
+public class Ad {
+    private int ID;
+    private int Price;
+    private String Brand;
+    private String Model;
+    private String Type;
+    private int Year;
+    private String Engine;
+    private String Transmission;
+    private String Drive_unit;
+    private int Mileage;
+    private String Color;
+    private int FK_Users;
+}
